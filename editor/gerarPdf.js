5bf5d2c82a7393769b5a9ec100b1e4355b390a28
hiago <souza8hiago@gmail.com>
2025-10-02 22:05:52 -0300

    update

--- editor/gerarPdf.js
+++ editor/gerarPdf.js
@@ -1,1 +1,22 @@
-let frConteudo = document.getElementById('meuIframe');
+let iframe = document.getElementById("meuIframe");
+function gerarPdf() {
+  const conteudo = iframe.contentDocument || iframe.contentWindow.document;
+  conteudo.body.style.width = "800px";
+
+  let bgOriginal = conteudo.body.style.backgroundImage;
+  conteudo.body.style.backgroundImage = "none";
+
+  html2pdf()
+    .set({
+      margin: [30, 20, 20, 30], // superior, direita, inferior, esquerda em mm
+      filename: "curriculo_abnt.pdf",
+      html2canvas: { scale: 2, backgroundColor: "#ffffff" },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    })
+    .from(conteudo.body)
+    .save()
+    .then(() => {
+      // 🔹 Restaura o fundo no preview
+      conteudo.body.style.backgroundImage = bgOriginal;
+    });
+}
